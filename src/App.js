e6be3e30f81ed52e262e920956951e0a1aa33348
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-06-23 19:38:56 +0500

    work complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-
+import {Download, Features,SectionWrapper} from './components'
 
 const App=()=> {
   return (
     <div className="text-3xl font-bold underline">
      <h1>React Native Show Case</h1>
+     <Download />
+     <Features />
+     <SectionWrapper />
     </div>
   );
 }
