eaf536addb1ae8d3e2603c6d610f6aeddc6d1bed
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-06-25 18:46:56 +0500

    work complete

--- src/components/SectionWrapper.jsx
+++ src/components/SectionWrapper.jsx
@@ -5,6 +5,11 @@ import assets from '../assets'
 const SectionWrapper = ({title, description, showBtn, mockupImg, banner}) => {
   return (
     <div className={`min-h-screen ${styles.section} ${styles.bgWhite} ${banner}`}>
+      <div className={`flex items-center ${styles.boxClass}`}>
+        <div classname={`${styles.descDiv}`}>
+
+        </div>
+      </div>
      <h1>{title}</h1>
      <h1>{description}</h1>
      <img src={mockupImg} alt="MockUp"/>
