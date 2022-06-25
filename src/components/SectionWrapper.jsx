d81258cbbac0c3c43ec5a33e9e3d0a3972e8eed3
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-06-25 18:42:56 +0500

    work complete

--- src/components/SectionWrapper.jsx
+++ src/components/SectionWrapper.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import styles from '../styles/Global'
+import assets from '../assets'
 
-const SectionWrapper = ({title, description, showBtn, mockupImg}) => {
+const SectionWrapper = ({title, description, showBtn, mockupImg, banner}) => {
   return (
-    <div>
+    <div className={`min-h-screen ${styles.section} ${styles.bgWhite} ${banner}`}>
      <h1>{title}</h1>
      <h1>{description}</h1>
      <img src={mockupImg} alt="MockUp"/>
