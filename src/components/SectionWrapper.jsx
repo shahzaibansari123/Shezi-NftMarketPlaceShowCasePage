f2018481c7872281b8c1c6ba1c94d4ed541cbcb4
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-06-28 15:09:32 +0500

    work complete

--- src/components/SectionWrapper.jsx
+++ src/components/SectionWrapper.jsx
@@ -6,17 +6,16 @@ import Button from './Button'
 const SectionWrapper = ({ title, description, showBtn, mockupImg, banner }) => {
   return (
     <div className={`min-h-screen ${styles.section} ${styles.bgWhite} ${banner}`}>
-      <div className={`flex items-center ${styles.boxClass}`}>
-        <div classname={`${styles.descDiv}`}>
+      <div className={`flex items-center ${styles.boxClass} w-11/12 sm:w-full minmd:w-3/4`}>
+        <div className={`${styles.descDiv}`}>
           <h1 className={`${styles.h1Text}`}>{title}</h1>
           <p className={`${styles.descriptionText}`}>{description}</p>
           {showBtn && (
             <Button assetUrl={assets.expo} link="deployed app" />
           )}
         </div>
-        <div className={`flex-1 ${styles.flexCenter}`}>
-      <img src={mockupImg} alt="MockUp" />
-
+        <div className={`flex-1 ${styles.flexCenter} p-8 sm:px-0`}>
+          <img src={mockupImg} alt="MockUp" className={styles.sectionImg} />
         </div>
       </div>
     </div>
