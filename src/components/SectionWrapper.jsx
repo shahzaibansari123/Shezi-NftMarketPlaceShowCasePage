ab2c61c4f6e26759d40617225b2904dc874e4a69
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-06-30 15:59:24 +0500

    work complete

--- src/components/SectionWrapper.jsx
+++ src/components/SectionWrapper.jsx
@@ -1,25 +1,38 @@
-import React from 'react'
-import styles from '../styles/Global'
-import assets from '../assets'
-import Button from './Button'
+import React from "react";
+import styles from "../styles/Global";
+import assets from "../assets";
+import Button from "./Button";
 
-const SectionWrapper = ({ title, description, showBtn, mockupImg, banner, reverse }) => {
+const SectionWrapper = ({
+  title,
+  description,
+  showBtn,
+  mockupImg,
+  banner,
+  reverse,
+}) => {
   return (
-    <div className={`min-h-screen ${styles.section} ${styles.bgWhite} ${banner}`}>
-      <div className={`flex items-center ${styles.boxClass} w-11/12 sm:w-full minmd:w-3/4`}>
+    <div
+      className={`min-h-screen 
+      ${styles.section} 
+      ${styles.bgWhite} 
+      ${reverse ? styles.bgWhite : styles.bgPrimary} 
+      ${banner}`}
+    >
+      <div
+        className={`flex items-center ${styles.boxClass} w-11/12 sm:w-full minmd:w-3/4`}
+      >
         <div className={`${styles.descDiv}`}>
           <h1 className={`${styles.h1Text}`}>{title}</h1>
           <p className={`${styles.descriptionText}`}>{description}</p>
-          {showBtn && (
-            <Button assetUrl={assets.expo} link="deployed app" />
-          )}
+          {showBtn && <Button assetUrl={assets.expo} link="deployed app" />}
         </div>
         <div className={`flex-1 ${styles.flexCenter} p-8 sm:px-0`}>
           <img src={mockupImg} alt="MockUp" className={styles.sectionImg} />
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default SectionWrapper
+export default SectionWrapper;
