d471befac2b3565ce24cb86badcd58d2c8294d8e
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-07-01 06:00:06 +0500

    work complete

--- src/components/SectionWrapper.jsx
+++ src/components/SectionWrapper.jsx
@@ -23,7 +23,9 @@ const SectionWrapper = ({
         className={`flex items-center ${reverse ? styles.boxReverseClass : styles.boxClass} w-11/12 sm:w-full minmd:w-3/4`}
       >
         <div className={`${styles.descDiv} ${reverse ? " fadeRightMini" : " fadeLeftMini"} ${reverse ?  styles.textRight : styles.textLeft}`}>
-          <h1 className={`${styles.h1Text}`}>{title}</h1>
+          <h1 className={` ${reverse ? styles.blackText : styles.whiteText} ${styles.h1Text}`}>
+            {title}
+            </h1>
           <p className={`${styles.descriptionText}`}>{description}</p>
           {showBtn && <Button assetUrl={assets.expo} link="deployed app" />}
         </div>
