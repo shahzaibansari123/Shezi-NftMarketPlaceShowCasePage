72d6e9bba63069f3267db34953579ebd1376d135
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-07-06 01:21:01 +0500

    work complete

--- src/components/Download.jsx
+++ src/components/Download.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
-import styles from './styles/Global'
+import styles from '../styles/Global'
 import assets from '../assets'
 
 const Download = () => {
   return (
-    <div>
-      Download
+    <div className={`${styles.section} ${styles.bgWhite}`}>
+      <div className={`${styles.subSection } flex-col text-center`}> 
+      <div>
+        <h1>Download the Souce Code</h1>
+        <p>Get the full Source code on Github</p>
+
+      </div>
+
+      </div>
     </div>
   )
 }
