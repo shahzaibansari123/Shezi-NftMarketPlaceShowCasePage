0d5bd34455836ecea996db5789916e08eb42d15b
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-07-07 15:22:54 +0500

    work complete

--- src/components/Download.jsx
+++ src/components/Download.jsx
@@ -7,8 +7,8 @@ const Download = () => {
     <div className={`${styles.section} ${styles.bgWhite}`}>
       <div className={`${styles.subSection } flex-col text-center`}> 
       <div>
-        <h1>Download the Souce Code</h1>
-        <p>Get the full Source code on Github</p>
+        <h1 className={`${styles.h1Text} ${styles.blackText}`}>Download the Souce Code</h1>
+        <p className={`${styles.pText} ${styles.blackText}`}>Get the full Source code on Github</p>
 
       </div>
 
